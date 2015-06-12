a5b7ac402c696186de8b314dacb25afc2976b110
Balbir Sanghera <b.sanghera@partner.samsung.com>
2015-06-12 11:25:27 +0100

    Constants for BLE and notification for disconnect

---
+++ app/src/main/java/com/samsung/microbit/model/Constants.java
@@ -0,0 +1,67 @@
+package com.samsung.microbit.model;
+
+public class Constants {
+
+	public static final int REMOTE_CONTROL_PLAY = 0;
+	public static final int REMOTE_CONTROL_PAUSE = 1;
+	public static final int REMOTE_CONTROL_STOP = 2;
+	public static final int REMOTE_CONTROL_NEXTTRACK = 3;
+	public static final int REMOTE_CONTROL_PREVTRACK = 4;
+	public static final int REMOTE_CONTROL_FORWARD = 5;
+	public static final int REMOTE_CONTROL_REWIND = 6;
+	public static final int REMOTE_CONTROL_VOLUMEUP = 7;
+	public static final int REMOTE_CONTROL_VOLUMEDOWN = 8;
+
+	public static final int CAMERA_LAUNCH_PHOTO_MODE = 0;
+	public static final int CAMERA_LAUNCH_VIDEO_MODE = 1;
+	public static final int CAMERA_TAKE_PHOTO = 2;
+	public static final int CAMERA_START_VIDEO_CAPTURE = 3;
+	public static final int CAMERA_STOP_VIDEO_CAPTURE = 4;
+
+	public static final int AUDIO_RECORDER_LAUNCH = 0;
+	public static final int AUDIO_RECORDER_START_CAPTURE = 1;
+	public static final int AUDIO_RECORDER_STOP_CAPTURE = 2;
+
+	public static final int ALERT_DISPLAY_TOAST = 0;
+	public static final int ALERT_VIBRATE = 1;
+	public static final int ALERT_PLAY_SOUND = 2;
+	public static final int ALERT_PLAY_RINGTONE = 3;
+	public static final int FIND_MY_PHONE = 4;
+
+	public static final int ALARM_SOUND = 0;
+	public static final int SOUND_TBD_1 = 1;
+	public static final int SOUND_TBD_2 = 2;
+	public static final int SOUND_TBD_3 = 3;
+	public static final int SOUND_TBD_4 = 4;
+	public static final int SOUND_TBD_5 = 5;
+
+	public static final int BUTTON_A_UP = 0;
+	public static final int BUTTON_A_DOWN = 1;
+	public static final int BUTTON_B_UP = 2;
+	public static final int BUTTON_B_DOWN = 3;
+	public static final int BUTTON_A_LONG_PRESS = 4;
+	public static final int BUTTON_B_LONG_PRESS = 5;
+	public static final int BOTH_BUTTONS_PRESSED = 6;
+
+	public static final int SIGNAL_STRENGTH_NO_BAR = 0;
+	public static final int SIGNAL_STRENGTH_ONE_BAR = 1;
+	public static final int SIGNAL_STRENGTH_TWO_BAR = 2;
+	public static final int SIGNAL_STRENGTH_THREE_BAR = 3;
+	public static final int SIGNAL_STRENGTH_FOUR_BAR = 4;
+
+	public static final int DEVICE_ORIENTATION_LANDSCAPE = 0;
+	public static final int DEVICE_ORIENTATION_PORTRAIT = 1;
+
+	public static final int DEVICE_GESTURE_NONE = 0;
+	public static final int DEVICE_GESTURE_DEVICE_SHAKEN = 1;
+
+	public static final int DEVICE_DISPLAY_OFF = 0;
+	public static final int DEVICE_DISPLAY_ON= 1;
+
+	public static final int BUTTON_UP = 0;
+	public static final int BUTTON_DOWN = 1;
+	public static final int BUTTON_RIGHT = 2;
+	public static final int BUTTON_LEFT = 3;
+	public static final int BUTTON_A = 4;
+	public static final int BUTTON_B = 5;
+}
