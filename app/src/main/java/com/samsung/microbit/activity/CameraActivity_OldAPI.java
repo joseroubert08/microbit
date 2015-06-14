e076fc38555fccf9c285f856dd6342e2f420ee3d
t.maestri <t.maestri@samsung.com>
2015-06-14 14:28:42 +0100

    Added Video Recording Support for old API

--- app/src/main/java/com/samsung/microbit/activity/CameraActivity_OldAPI.java
+++ app/src/main/java/com/samsung/microbit/activity/CameraActivity_OldAPI.java
@@ -11,6 +11,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
@@ -20,23 +22,16 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Message;
-import android.os.RemoteException;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
-import com.samsung.microbit.MBApp;
 import com.samsung.microbit.R;
 import com.samsung.microbit.model.CmdArg;
 import com.samsung.microbit.plugin.CameraPlugin;
@@ -47,12 +42,14 @@ import com.samsung.microbit.service.PluginService;
  */
 public class CameraActivity_OldAPI extends Activity {
 
-	CameraPreview preview;
-	Button buttonClick;
-	Camera camera;
-	Activity act;
-	Context ctx;
+	private CameraPreview mPreview;
+	private Button mButtonClick;
+	private Camera mCamera;
 	private BroadcastReceiver mMessageReceiver;
+	private boolean mVideo = false;
+	private boolean mIsRecording = false;
+	private MediaRecorder mMediaRecorder;
+	private File mVideoFile = null;
 
 	private static final String TAG = "CameraActivity_OldAPI";
 	private boolean debug = true;
@@ -60,24 +57,6 @@ public class CameraActivity_OldAPI extends Activity {
 	void logi(String message) {
 		if (debug) {
 			Log.i(TAG, "### " + Thread.currentThread().getId() + " # " + message);
-		}
-	}
-
-
-	public static void sendReplyCommand(int mbsService, CmdArg cmd) {
-
-		if (PluginService.mClientMessenger != null) {
-			Message msg = Message.obtain(null, mbsService);
-			Bundle bundle = new Bundle();
-			bundle.putInt("cmd", cmd.getCMD());
-			bundle.putString("value", cmd.getValue());
-			msg.setData(bundle);
-
-			try {
-				PluginService.mClientMessenger.send(msg);
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
@@ -94,40 +73,19 @@ public class CameraActivity_OldAPI extends Activity {
 		return -1;
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	private void setButtonForPicture() {
 
-		logi("onCreate() :: Start");
-		super.onCreate(savedInstanceState);
-
-		ctx = this;
-		act = this;
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.camera_old_api);
-		preview = new CameraPreview(this, (SurfaceView) findViewById(R.id.surfaceView));
-		preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-		((FrameLayout) findViewById(R.id.camera_preview_layout)).addView(preview);
-		preview.setKeepScreenOn(true);
-		preview.setOnClickListener(new OnClickListener() {
-
-			@Override
+		mButtonClick.setText(R.string.picture);
+		mButtonClick.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+				mCamera.takePicture(shutterCallback, rawCallback, jpegCallback);
 			}
 		});
 
-		buttonClick = (Button) findViewById(R.id.picture);
-		buttonClick.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-			}
-		});
-
-		buttonClick.setOnLongClickListener(new OnLongClickListener() {
+		mButtonClick.setOnLongClickListener(new OnLongClickListener() {
 			@Override
 			public boolean onLongClick(View arg0) {
-				camera.autoFocus(new AutoFocusCallback() {
+				mCamera.autoFocus(new AutoFocusCallback() {
 					@Override
 					public void onAutoFocus(boolean arg0, Camera arg1) {
 						//TODO Is there anything we have to do after autofocus?
@@ -137,15 +95,129 @@ public class CameraActivity_OldAPI extends Activity {
 			}
 		});
 
+	}
+
+	private void setPreviewForPicture() {
+		mPreview.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mCamera.autoFocus(new AutoFocusCallback() {
+					@Override
+					public void onAutoFocus(boolean arg0, Camera arg1) {
+						//TODO Is there anything we have to do after autofocus?
+					}
+				});
+			}
+		});
+	}
+
+	private void setButtonForVideo() {
+
+		mButtonClick.setText(R.string.record);
+		mButtonClick.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (mIsRecording) {
+					// stop recording and release mCamera
+					mMediaRecorder.stop(); // stop the recording
+					refreshGallery(mVideoFile);
+					releaseMediaRecorder(); // release the MediaRecorder object
+					mIsRecording = false;
+					finish();
+				} else {
+					if (!prepareMediaRecorder()) {
+						sendCameraError();
+						Log.e(TAG,"Error preparing mediaRecorder");
+						finish();
+					}
+
+					mButtonClick.setText(R.string.stop);
+
+					//TODO Check that is true
+					// work on UiThread for better performance
+					runOnUiThread(new Runnable() {
+						public void run() {
+							try {
+								mMediaRecorder.start();
+							} catch (final Exception ex) {
+								sendCameraError();
+								//TODO Check that can be used
+								Log.e(TAG, "Error during video recording");
+								finish();
+							}
+						}
+					});
+
+					mIsRecording = true;
+				}
+			}
+		});
+	}
+
+	private void setPreviewForVideo() {
+
+	}
+
+	private void sendCameraError() {
+		CmdArg cmd = new CmdArg(0,"Camera Error");
+		CameraPlugin.sendReplyCommand(PluginService.CAMERA, cmd);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		logi("onCreate() :: Start");
+		super.onCreate(savedInstanceState);
+
+		mCamera = null;
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.camera_old_api);
+		Intent intent = getIntent();
+		if(intent.getAction().contains("OPEN_FOR_PIC")) {
+			mVideo = false;
+		}else if(intent.getAction().contains("OPEN_FOR_VIDEO")) {
+			mVideo = true;
+		}
+		mPreview = new CameraPreview(this, mCamera);
+		((FrameLayout) findViewById(R.id.surfaceView)).addView(mPreview);
+
+		mPreview.setKeepScreenOn(true);
+
+		mButtonClick = (Button) findViewById(R.id.picture);
+
+
+		if(mVideo) {
+			//Setup specific to OPEN_FOR_VIDEO
+			setButtonForVideo();
+			setPreviewForVideo();
+		}
+		else{
+			//Setup specific to OPEN_FOR_PIC
+			setButtonForPicture();
+			setPreviewForPicture();
+		}
+
 		mMessageReceiver = new BroadcastReceiver() {
 
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				Log.i("CameraActivity_OldAPI", "mMessageReceiver.onReceive() :: Start");
 
-				if (intent.getAction().equals("TAKE_PIC")) {
-					//TODO Complete with the correct code
-					buttonClick.callOnClick();
+				if (!mVideo && intent.getAction().equals("TAKE_PIC")) {
+					mButtonClick.callOnClick();
+				}
+				else if(mVideo && !mIsRecording && intent.getAction().equals("START_VIDEO")) {
+					mButtonClick.callOnClick();
+				}
+				else if(mVideo && mIsRecording && intent.getAction().equals("STOP_VIDEO")) {
+					mButtonClick.callOnClick();
+				}
+				else {
+					//Wrong sequence of commands
+					CmdArg cmd = new CmdArg(0,"Wrong Camera Command Sequence");
+					CameraPlugin.sendReplyCommand(PluginService.CAMERA, cmd);
+					Log.e(TAG, "Wrong command sequence");
 				}
 			}
 		};
@@ -154,7 +226,7 @@ public class CameraActivity_OldAPI extends Activity {
 	}
 
 
-//    public int getCameraDisplayOrientation(int cameraId, android.hardware.Camera camera)
+//    public int getCameraDisplayOrientation(int cameraId, android.hardware.Camera mCamera)
 //    {
 //        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
 //        android.hardware.Camera.getCameraInfo(cameraId, info);
@@ -198,21 +270,29 @@ public class CameraActivity_OldAPI extends Activity {
 		super.onResume();
 		int camIdx = getFrontFacingCamera();
 		try {
-			camera = Camera.open(camIdx);
-			//int mRotation = getCameraDisplayOrientation(camIdx,camera);
-			Camera.Parameters parameters = camera.getParameters();
+			mCamera = Camera.open(camIdx);
+			//int mRotation = getCameraDisplayOrientation(camIdx,mCamera);
+			Camera.Parameters parameters = mCamera.getParameters();
 			parameters.setRotation(270); //set rotation to save the picture
-			camera.setParameters(parameters);
-			camera.setDisplayOrientation(90);
-			camera.startPreview();
-			preview.setCamera(camera);
+			mCamera.setParameters(parameters);
+			mCamera.setDisplayOrientation(90);
+			mCamera.startPreview();
+			mPreview.setCamera(mCamera);
 			//LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("TAKE_PIC"));
 
-			this.registerReceiver(mMessageReceiver, new IntentFilter("TAKE_PIC"));
+			if(mVideo){
+				this.registerReceiver(mMessageReceiver, new IntentFilter("START_VIDEO"));
+				this.registerReceiver(mMessageReceiver, new IntentFilter("STOP_VIDEO"));
+			}
+			else {
+				this.registerReceiver(mMessageReceiver, new IntentFilter("TAKE_PIC"));
+			}
 
 			logi("onCreate() :: onResume # ");
 		} catch (RuntimeException ex) {
-			Toast.makeText(ctx, getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
+			Toast.makeText(this, getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
+			sendCameraError();
+			finish();
 		}
 	}
 
@@ -221,19 +301,19 @@ public class CameraActivity_OldAPI extends Activity {
 
 		logi("onCreate() :: onPause");
 		this.unregisterReceiver(
-			mMessageReceiver);
-		if (camera != null) {
-			camera.stopPreview();
-			preview.setCamera(null);
-			camera.release();
-			camera = null;
+				mMessageReceiver);
+		if (mCamera != null) {
+			mCamera.stopPreview();
+			mPreview.setCamera(null);
+			mCamera.release();
+			mCamera = null;
 		}
 		super.onPause();
 	}
 
 	private void resetCam() {
-		camera.startPreview();
-		preview.setCamera(camera);
+		mCamera.startPreview();
+		mPreview.setCamera(mCamera);
 	}
 
 	private void refreshGallery(File file) {
@@ -266,16 +346,15 @@ public class CameraActivity_OldAPI extends Activity {
 
 	@Override
 	protected void onStart() {
-		//Informing microbit that the camera is active now
+		//Informing microbit that the mCamera is active now
 		CmdArg cmd = new CmdArg(0, "Camera on");
 		CameraPlugin.sendReplyCommand(PluginService.CAMERA, cmd);
-		Log.d(TAG, "Sent message CameraOn to microbit");
 		super.onStart();
 	}
 
 	@Override
 	protected void onDestroy() {
-		//Informing microbit that the camera is active now
+		//Informing microbit that the mCamera is active now
 		CmdArg cmd = new CmdArg(0, "Camera off");
 		CameraPlugin.sendReplyCommand(PluginService.CAMERA, cmd);
 		super.onDestroy();
@@ -289,10 +368,13 @@ public class CameraActivity_OldAPI extends Activity {
 
 			// Write to SD Card
 			try {
-				File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+				File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/Microbit/");
+
+				if(!dir.exists())
+					dir.mkdirs();
 
 				//TODO defining the file name
-				String fileName = String.format("microbit_%d.jpg", System.currentTimeMillis());
+				String fileName = String.format("%d.jpg", System.currentTimeMillis());
 				File outFile = new File(dir, fileName);
 
 				outStream = new FileOutputStream(outFile);
@@ -312,6 +394,61 @@ public class CameraActivity_OldAPI extends Activity {
 			}
 			return null;
 		}
+
+	}
+
+	private void releaseMediaRecorder() {
+		if (mMediaRecorder != null) {
+			mMediaRecorder.reset(); // clear recorder configuration
+			mMediaRecorder.release(); // release the recorder object
+			mMediaRecorder = null;
+			mVideoFile = null;
+			//TODO Check that is not necessary
+			//mCamera.lock(); // lock camera for later use
+		}
+	}
+
+	private boolean prepareMediaRecorder() {
+
+		mMediaRecorder = new MediaRecorder();
+
+		mCamera.unlock();
+		mMediaRecorder.setCamera(mCamera);
+
+		mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+		mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+		try {
+			//TODO Check that the video quality is enough
+			mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+
+			//TODO defining the file name
+			File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/Microbit/");
+
+			if(!dir.exists())
+				dir.mkdirs();
+
+			//TODO defining the file name
+			String fileName = String.format("%d.mp4", System.currentTimeMillis());
+			mVideoFile = new File(dir, fileName);
+
+			mMediaRecorder.setOutputFile(mVideoFile.getAbsolutePath());
+
+			//TODO Check File Limits
+			mMediaRecorder.setMaxDuration(600000); // Set max duration 60 sec.
+			mMediaRecorder.setMaxFileSize(50000000); // Set max file size 50M
+			mMediaRecorder.setOrientationHint(270);
+			mMediaRecorder.prepare();
+		} catch (IllegalStateException e) {
+			releaseMediaRecorder();
+			Log.e(TAG,"Error preparing media Recorder: " + e.getLocalizedMessage());
+			return false;
+		} catch (IOException e) {
+			releaseMediaRecorder();
+			Log.e(TAG, "Error preparing media Recorder IOException: " + e.getLocalizedMessage());
+			return false;
+		}
+		return true;
 
 	}
 }
