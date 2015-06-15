18c4b0b972d5dd1ee297e11f2444be031587364a
t.maestri <t.maestri@samsung.com>
2015-06-15 18:04:57 +0100

    Fixed camera preview ratio bug

--- app/src/main/java/com/samsung/microbit/activity/CameraPreview.java
+++ app/src/main/java/com/samsung/microbit/activity/CameraPreview.java
@@ -54,65 +54,65 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         }
     }
 
-//    @Override
-//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        // We purposely disregard child measurements because act as a
-//        // wrapper to a SurfaceView that centers the camera preview instead
-//        // of stretching it.
-//        Log.d(TAG,"onMeasure(" + widthMeasureSpec + "," + heightMeasureSpec + ")");
-//
-//        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
-//        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
-//        setMeasuredDimension(width, height);
-//
-//        if (mSupportedPreviewSizes != null) {
-//            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
-//        }
-//    }
-//
-//    @Override
-//    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-//        Log.d(TAG,"onLayout()");
-//        if (changed) {
-//            final View child = mSurfaceView;
-//
-//            final int width = r - l;
-//            final int height = b - t;
-//
-//            Log.d(TAG,"onLayout() wxh = " + width + " x " + height);
-//
-//            int previewWidth = width;
-//            int previewHeight = height;
-//            int inverseRadio = height/width;
-//
-//            int previewRatio = 0;
-//
-//            if (mPreviewSize != null) {
-//                previewWidth = mPreviewSize.width;
-//                previewHeight = mPreviewSize.height;
-//                previewRatio = previewWidth/previewHeight;
-//            }
-//
-//            if (Math.abs(inverseRadio - previewRatio) < 0.001)
-//            {
-//                previewWidth = mPreviewSize.height;
-//                previewHeight = mPreviewSize.width;
-//            }
-//
-//            Log.d(TAG,"onLayout() previewWidth x previewHeight = " + previewWidth + " x " + previewHeight);
-//
-//            // Center the child SurfaceView within the parent.
-//            if (width * previewHeight > height * previewWidth) {
-//                final int scaledChildWidth = previewWidth * height / previewHeight;
-//                child.layout((width - scaledChildWidth) / 2, 0,
-//                        (width + scaledChildWidth) / 2, height);
-//            } else {
-//                final int scaledChildHeight = previewHeight * width / previewWidth;
-//                child.layout(0, (height - scaledChildHeight) / 2,
-//                        width, (height + scaledChildHeight) / 2);
-//            }
-//        }
-//    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // We purposely disregard child measurements because act as a
+        // wrapper to a SurfaceView that centers the camera preview instead
+        // of stretching it.
+        Log.d(TAG,"onMeasure(" + widthMeasureSpec + "," + heightMeasureSpec + ")");
+
+        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        setMeasuredDimension(width, height);
+
+        if (mSupportedPreviewSizes != null) {
+            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Log.d(TAG,"onLayout()");
+        if (changed) {
+            //final View child = mSurfaceView;
+
+            final int width = r - l;
+            final int height = b - t;
+
+            Log.d(TAG,"onLayout() wxh = " + width + " x " + height);
+
+            int previewWidth = width;
+            int previewHeight = height;
+            int inverseRadio = height/width;
+
+            int previewRatio = 0;
+
+            if (mPreviewSize != null) {
+                previewWidth = mPreviewSize.width;
+                previewHeight = mPreviewSize.height;
+                previewRatio = previewWidth/previewHeight;
+            }
+
+            if (Math.abs(inverseRadio - previewRatio) < 0.001)
+            {
+                previewWidth = mPreviewSize.height;
+                previewHeight = mPreviewSize.width;
+            }
+
+            Log.d(TAG,"onLayout() previewWidth x previewHeight = " + previewWidth + " x " + previewHeight);
+
+            // Center the child SurfaceView within the parent.
+            if (width * previewHeight > height * previewWidth) {
+                final int scaledChildWidth = previewWidth * height / previewHeight;
+                this.layout((width - scaledChildWidth) / 2, 0,
+                        (width + scaledChildWidth) / 2, height);
+            } else {
+                final int scaledChildHeight = previewHeight * width / previewWidth;
+                this.layout(0, (height - scaledChildHeight) / 2,
+                        width, (height + scaledChildHeight) / 2);
+            }
+        }
+    }
 
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, acquire the camera and tell it where
@@ -141,10 +141,10 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         if(mCamera != null) {
             mCamera.stopPreview();
-//            Camera.Parameters parameters = mCamera.getParameters();
-//            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
             requestLayout();
-//            mCamera.setParameters(parameters);
+            mCamera.setParameters(parameters);
             try {
                 mCamera.setPreviewDisplay(mHolder);
                 mCamera.startPreview();
@@ -154,47 +154,47 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         }
     }
 
-//    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-//        final double ASPECT_TOLERANCE = 0.1;
-//        if(w<h) {
-//            int temp = w;
-//            w = h;
-//            h = temp;
-//        }
-//
-//        double targetRatio = (double) w / h;
-//        if (sizes == null) return null;
-//
-//        Size optimalSize = null;
-//        double minDiff = Double.MAX_VALUE;
-//
-//        int targetHeight = h;
-//
-//        // Try to find an size match aspect ratio and size
-//        for (Size size : sizes) {
-//            double ratio = (double) size.width / size.height;
-//            Log.d(TAG,"current wxh = " + size.width + " x " + size.height);
-//            Log.d(TAG,"targetRatio = " + ratio);
-//            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-//            if (Math.abs(size.height - targetHeight) < minDiff) {
-//                optimalSize = size;
-//                minDiff = Math.abs(size.height - targetHeight);
-//            }
-//        }
-//
-//        // Cannot find the one match the aspect ratio, ignore the requirement
-//        if (optimalSize == null) {
-//            minDiff = Double.MAX_VALUE;
-//            for (Size size : sizes) {
-//                if (Math.abs(size.height - targetHeight) < minDiff) {
-//                    optimalSize = size;
-//                    minDiff = Math.abs(size.height - targetHeight);
-//                }
-//            }
-//        }
-//
-//        Log.d(TAG,"optimalSize = " + optimalSize.width + " x " + optimalSize.height);
-//
-//        return optimalSize;
-//    }
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        if(w<h) {
+            int temp = w;
+            w = h;
+            h = temp;
+        }
+
+        double targetRatio = (double) w / h;
+        if (sizes == null) return null;
+
+        Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            Log.d(TAG,"current wxh = " + size.width + " x " + size.height);
+            Log.d(TAG,"targetRatio = " + ratio);
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+
+        Log.d(TAG,"optimalSize = " + optimalSize.width + " x " + optimalSize.height);
+
+        return optimalSize;
+    }
 }
