9dcd0e27c1ccb4faf0fb968951bc1eade07b4726
kkulendiran <kkulendiran@samsung.com>
2015-06-22 17:51:38 +0100

    UI for initial view added

---
+++ app/src/main/java/com/samsung/microbit/ui/activity/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.samsung.microbit.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.samsung.microbit.MBApp;
+import com.samsung.microbit.R;
+
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getResources().getBoolean(R.bool.portrait_only)){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        MBApp.setContext(this);
+
+        setContentView(R.layout.activity_home);
+
+        RelativeLayout connectBarView = (RelativeLayout) findViewById(R.id.connectBarView);
+        connectBarView.getBackground().setAlpha(128);
+
+        RelativeLayout projectBarView = (RelativeLayout) findViewById(R.id.projectBarView);
+        projectBarView.getBackground().setAlpha(128);
+    }
+
+    public void onBtnClicked(View v){
+        if(v.getId() == R.id.addDevice){
+            Intent intent = new Intent(this, ConnectActivity.class);
+            startActivity(intent);
+        }
+    }
+}
