cb8a75445695dae94333b56a0861ba8c126f4752
Anton Obzhirov <a.obzhirov@samsung.com>
2015-06-23 18:40:01 +0100

    Add connectBarTitle and projectBarProjects updates.

--- app/src/main/java/com/samsung/microbit/ui/activity/HomeActivity.java
+++ app/src/main/java/com/samsung/microbit/ui/activity/HomeActivity.java
@@ -2,15 +2,18 @@ package com.samsung.microbit.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.samsung.microbit.MBApp;
 import com.samsung.microbit.R;
+import com.samsung.microbit.core.Utils;
 import com.samsung.microbit.model.Constants;
 
 public class HomeActivity extends Activity {
@@ -34,8 +37,12 @@ public class HomeActivity extends Activity {
         RelativeLayout connectBarView = (RelativeLayout) findViewById(R.id.connectBarView);
         connectBarView.getBackground().setAlpha(128);
 
+        updateConnectBarTitle();
+
         RelativeLayout projectBarView = (RelativeLayout) findViewById(R.id.projectBarView);
         projectBarView.getBackground().setAlpha(128);
+
+        updateProjectBarProjects();
     }
 
     public void onBtnClicked(View v){
@@ -48,4 +55,23 @@ public class HomeActivity extends Activity {
             startActivity(intent);
         }
     }
+
+    private final void updateConnectBarTitle()
+    {
+        TextView connectBarTitle = (TextView) findViewById(R.id.title);
+        SharedPreferences p = Utils.getInstance().getPreferences(this);
+        connectBarTitle.setText(p.getString(Utils.PREFERENCES_NAME_KEY, "Connect to your Micro:Bit"));
+    }
+
+    private final void updateProjectBarProjects()
+    {
+        TextView projectBarProjects = (TextView) findViewById(R.id.projects);
+        projectBarProjects.setText(Integer.toString(Utils.findProgramsAndPopulate(null, null)) + " saved projects");
+    }
+
+    public void onResume() {
+        super.onResume();
+
+    }
+
 }
