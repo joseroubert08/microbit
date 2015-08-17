4344f0ef5ac29dc370e6b9531ba1f7fc708ba5a5
frederic.ma <frederic.ma@samsung.com>
2015-08-17 08:07:15 +0100

    Fixed camera close command

--- app/src/main/java/com/samsung/microbit/plugin/CameraPlugin.java
+++ app/src/main/java/com/samsung/microbit/plugin/CameraPlugin.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
 import android.os.RemoteException;
-import android.widget.Toast;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.samsung.microbit.model.Constants;
@@ -77,8 +75,6 @@ public class CameraPlugin {
 	//This function should trigger an Activity that would be responsible of starting the camera app to take a picture.
 	//The same activity should also store the result of the camera app, if valid
 	private static void launchCameraForPic() {
-		//TODO: remove Toast
-		Toast.makeText(context, "Camera Activity Started", Toast.LENGTH_SHORT).show();
 		Intent mIntent = new Intent(context, CameraActivity_OldAPI.class);
 		mIntent.setAction("com.samsung.microbit.activity.CameraActivity.action.OPEN_FOR_PIC");
 		mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -86,15 +82,11 @@ public class CameraPlugin {
 	}
 
 	private static void takePic() {
-		//TODO: remove Toast
-		Toast.makeText(context, "Take Pic Start", Toast.LENGTH_SHORT).show();
 		Intent intent = new Intent("TAKE_PIC");
 		context.sendBroadcast(intent);
 	}
 
 	private static void launchCameraForVideo() {
-		//TODO: remove Toast
-		Toast.makeText(context, "Camera Activity Started", Toast.LENGTH_SHORT).show();
 		Intent mIntent = new Intent(context, CameraActivity_OldAPI.class);
 		mIntent.setAction("com.samsung.microbit.activity.CameraActivity.action.OPEN_FOR_VIDEO");
 		mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -102,23 +94,17 @@ public class CameraPlugin {
 	}
 
 	private static void recVideoStart() {
-		//TODO: remove Toast
-		Toast.makeText(context, "Rec Video Start", Toast.LENGTH_SHORT).show();
 		Intent intent = new Intent("START_VIDEO");
-		LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+		context.sendBroadcast(intent);
 	}
 
 	private static void recVideoStop() {
-		//TODO: remove Toast
-		Toast.makeText(context, "Rec Video Stop", Toast.LENGTH_SHORT).show();
 		Intent intent = new Intent("STOP_VIDEO");
-		LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+		context.sendBroadcast(intent);
 	}
 
 	private static void closeCamera() {
-		//TODO: remove Toast
-		Toast.makeText(context, "Close Camera", Toast.LENGTH_SHORT).show();
 		Intent intent = new Intent("CLOSE");
-		LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+		context.sendBroadcast(intent);
 	}
 }
