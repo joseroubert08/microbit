3dbcddc5a3723f55f9d88f2d909cc178ebc79610
Michael Akakpo <m.akakpo@partner.samsung.com>
2015-12-11 19:55:48 +0000

    [Accessibility] - adding changes for home and pairing screens

--- app/src/main/java/com/samsung/microbit/ui/activity/HomeActivity.java
+++ app/src/main/java/com/samsung/microbit/ui/activity/HomeActivity.java
@@ -147,12 +147,15 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private void setupDrawer() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // TODO: Accessibility Notifications check
+        toolbar.setNavigationContentDescription(R.string.content_description_toolbar_home);
         toolbar.setLogo(R.drawable.bbc_microbit);
+        toolbar.setLogoDescription(R.string.content_description_toolbar_logo);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-       // toggle.setDrawerIndicatorEnabled(false); // TODO: Change to the word 'Menu'
+        // toggle.setDrawerIndicatorEnabled(false); // TODO: Change to the word 'Menu'
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
