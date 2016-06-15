e3f6c6da118260f95174b4ca82cbf247b3ef6531
Andrii Hamula <andrii.hamula@globallogic.com>
2016-06-15 15:36:58 +0300

    [CHANGE] removed unused fields

--- app/src/main/java/com/samsung/microbit/MBApp.java
+++ app/src/main/java/com/samsung/microbit/MBApp.java
@@ -2,18 +2,12 @@ package com.samsung.microbit;
 
 import android.app.Application;
 import android.graphics.Typeface;
-import android.media.AudioAttributes;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.os.Build;
 import android.util.Log;
 
 import com.samsung.microbit.common.AppInfo;
 import com.samsung.microbit.core.EchoClientManager;
 
 public class MBApp extends Application {
-
-    private static final int MAX_STREAMS_SIMULTANIOUSLY = 10;
 
     private static MBApp app = null;
 
@@ -23,8 +17,6 @@ public class MBApp extends Application {
 
     private EchoClientManager echoClientManager;
 
-    private SoundPool appSoundPool;
-
     private AppInfo appInfo;
 
     @Override
@@ -33,19 +25,6 @@ public class MBApp extends Application {
         app = this;
         initTypefaces();
         echoClientManager = EchoClientManager.getInstance(this);
-
-        if(Build.VERSION.SDK_INT >= 21) {
-            AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
-            attrBuilder.setContentType(AudioAttributes.CONTENT_TYPE_MUSIC);
-            attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC);
-            attrBuilder.setUsage(AudioAttributes.USAGE_MEDIA);
-            attrBuilder.setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED);
-
-            appSoundPool = new SoundPool.Builder().setMaxStreams(MAX_STREAMS_SIMULTANIOUSLY).setAudioAttributes
-                    (attrBuilder.build()).build();
-        } else {
-            appSoundPool = new SoundPool(MAX_STREAMS_SIMULTANIOUSLY, AudioManager.STREAM_MUSIC, 0);
-        }
 
         appInfo = new AppInfo(this);
         Log.d("MBApp", "App Created");
@@ -79,11 +58,6 @@ public class MBApp extends Application {
     public EchoClientManager getEchoClientManager() {
         return echoClientManager;
     }
-
-    public SoundPool getAppSoundPool() {
-        return appSoundPool;
-    }
-
     public AppInfo getAppInfo() {
         return appInfo;
     }
