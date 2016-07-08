1de4b69da5c44e83c4950b4bf64dde0ab1ba237e
Oleksiy Tsubera <oleksiy.tsubera@globallogic.com>
2016-07-08 12:51:12 +0300

    Revert "Added a dialog window about disconnected micro:bit and ability to reconnect to it or dismiss. An affirmative button text on Step 2 pairing screen change from Next to Pair."
    This reverts commit 8859be8099d8b13d9b4f9137e2f8dfa3554be751.
    
    # Conflicts:
    #	Project/bbc_microbit/app/src/main/java/com/samsung/microbit/ui/activity/ProjectActivity.java

--- app/src/main/java/com/samsung/microbit/ui/activity/ProjectActivity.java
+++ app/src/main/java/com/samsung/microbit/ui/activity/ProjectActivity.java
@@ -3,7 +3,6 @@ package com.samsung.microbit.ui.activity;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -59,10 +58,6 @@ import no.nordicsemi.android.error.GattError;
 
 
 public class ProjectActivity extends Activity implements View.OnClickListener, IPCToBLEHelper.BLEBroadcastHandlable {
-    private static final String TAG = ProjectActivity.class.getSimpleName();
-
-    private static final int ALERT_DIALOG_RECONNECT = 1;
-
     private List<Project> mProjectList = new ArrayList<>();
     private ListView mProjectListView;
     private ListView mProjectListViewRight;
@@ -77,6 +72,7 @@ public class ProjectActivity extends Activity implements View.OnClickListener, I
     private DFUResultReceiver dfuResultReceiver;
 
     protected boolean debug = BuildConfig.DEBUG;
+    protected String TAG = ProjectActivity.class.getSimpleName();
 
     private List<Integer> mRequestPermissions = new ArrayList<>();
 
@@ -361,7 +357,7 @@ public class ProjectActivity extends Activity implements View.OnClickListener, I
                 }
             }
             break;
-        }
+         }
     }
 
     View.OnClickListener diskStoragePermissionCancelHandler = new View.OnClickListener() {
@@ -579,7 +575,7 @@ public class ProjectActivity extends Activity implements View.OnClickListener, I
     }
 
     /**
-     * Sends a project to flash on a mi0crobit board. If bluetooth is off then turn it on.
+     * Sends a project to flash on a micro:bit board. If bluetooth is off then turn it on.
      *
      * @param project Project to flash.
      */
@@ -767,52 +763,11 @@ public class ProjectActivity extends Activity implements View.OnClickListener, I
         }
     };
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        if (id == ALERT_DIALOG_RECONNECT) {
-            //Create dialog to reconnect to a micro:bit board after successful flashing.
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-            dialogBuilder.setTitle(R.string.reconnect_title);
-            dialogBuilder.setMessage(R.string.reconnect_text);
-            dialogBuilder.setPositiveButton(R.string.reconnect_ok_button, reconnectOnClickListener);
-            dialogBuilder.setNegativeButton(android.R.string.cancel, reconnectOnClickListener);
-            return dialogBuilder.create();
-        }
-        return super.onCreateDialog(id);
-    }
-
-    private DialogInterface.OnClickListener reconnectOnClickListener
-            = new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            switch (which) {
-                case Dialog.BUTTON_POSITIVE:
-                    //Do reconnect.
-                    toggleConnection();
-                    break;
-                case Dialog.BUTTON_NEGATIVE:
-                case Dialog.BUTTON_NEUTRAL:
-                    break;
-            }
-        }
-    };
-
     class DFUResultReceiver extends BroadcastReceiver {
 
         private boolean isCompleted = false;
         private boolean inInit = false;
         private boolean inProgress = false;
-
-        private View.OnClickListener popupFinishFlashingHandler = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logi("popupOkHandler");
-                PopUp.hide();
-
-                //Show dialog to reconnect to a board.
-                showDialog(ALERT_DIALOG_RECONNECT);
-            }
-        };
 
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -857,8 +812,8 @@ public class ProjectActivity extends Activity implements View.OnClickListener, I
                                         R.drawable.message_face, R.drawable.blue_btn,
                                         PopUp.GIFF_ANIMATION_NONE,
                                         PopUp.TYPE_ALERT, //type of popup.
-                                        popupFinishFlashingHandler,//override click listener for ok button
-                                        popupFinishFlashingHandler);//pass null to use default listener
+                                        popupOkHandler,//override click listener for ok button
+                                        popupOkHandler);//pass null to use default listener
                             }
 
                             isCompleted = true;
